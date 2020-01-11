3326b603823c9dd1e2ab73ed8ecc106401ed889c
夏文刚 <wengang.xia@funi365.com>
2020-01-11 21:19:24 +0800

    添加日志

--- src/main/java/com/xwg/metalg/web/api/controller/TestController.java
+++ src/main/java/com/xwg/metalg/web/api/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.xwg.metalg.web.api.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ajax")
 public class TestController {
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("/hello")
     public String sayHello() {
         String property = System.getProperty("name");
+        logger.error("property", property);
+        logger.error("property:" + property);
         return "{'message1': '" + property + "','message2','SpringBoot呵呵'}";
     }
 }
